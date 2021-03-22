05f59a272e2abf5bbcb7686f217256b331ecafd9
Laurent Lienher <laurent.lienher@camptocamp.com>
2021-03-22 17:59:18 +0100

    Not using javascript redirect but replace iframe content

--- main.py
+++ main.py
@@ -9,15 +9,31 @@ def homepage():
 
 @app.route('/captcha')
 def captcha():
-    return render_template('captcha.html')
+    id = request.args.get('id', None)
 
-@app.route('/data')
-def data():
-    return render_template('data.html')
+    if id is not None:
+        data = ''
+        return render_template('captcha.html', data=data)
+    else:
+        return render_template('error.html')
+
+# @app.route('/data')
+# def data():
+#     return render_template('data.html')
 
 @app.route('/verify', methods=['POST'])
 def verify():
-    token = json.loads(request.data)
+    data = json.loads(request.data)
+
+    if data['token']:
+        token = data['token']
+    else:
+        return render_template('captcha.html')
+
+    if data['id']:
+        id = data['id']
+    else:
+        return render_template('captcha.html')
     
     # FIXME: should be in yaml files
     verify_url = 'https://www.google.com/recaptcha/api/siteverify'
@@ -28,7 +44,9 @@ def verify():
     response = r.json()
 
     if response['success']:
-        redirect_url = url_for('data')
-        return redirect_url
+        data['id'] = id
+        data['value'] = 'donnée'
+        # redirect_url = url_for('data')
+        return render_template('captcha.html', data=data)
     else:
-        return 'NOK'
+        return render_template('captcha.html')
